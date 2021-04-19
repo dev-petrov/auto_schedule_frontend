02bb02a1ecccef4cee55fdbc658b2a3b16085ae8
Anton250 <antosha250900@gmail.com>
2021-04-19 12:37:42 +0300

    50 - added vue-select

--- src/components/modals/ModalGroup.vue
+++ src/components/modals/ModalGroup.vue
@@ -1,5 +1,5 @@
 <template lang="">
-    <b-modal id="modalGroup" :title="name.code" @hidden="closeModal()">
+    <b-modal id="modalGroup" @ok="saveGroup" :title="name.code" @hidden="closeModal()">
       <div class="d-block">
           <b-input-group size="md" :prepend="'Направление'">
             <b-form-input type="text" :value="name.training_direction.name"></b-form-input>
@@ -10,31 +10,74 @@
             <b-form-input type="number" :value="name.count_of_students"></b-form-input>
           </b-input-group>
         </div>
+        <div class="d-block">
+        <label>Дисциплины</label>
+        <v-select
+          multiple
+          v-model="disciplines"
+          id="disciplines"
+          :options="$store.state.disciplines"
+          placeholder="Выберите дисциплину"
+          label='title'
+          value='id'
+        ></v-select>
+      </div>
     </b-modal>
 </template>
 <script>
+import "vue-select/dist/vue-select.css";
+import vSelect from "vue-select";
+import http from '../../http';
 
 export default {
   props: ["groupID"],
+  components: {
+    vSelect,
+  },
   data() {
     return {
+      disciplines: [],
     };
   },
-  computed:{
-    name(){
-      return this.$store.state.groups[this.$store.state.groups.findIndex(v => v.id == this.groupID)] || {training_direction:{}}
+  computed: {
+    name() {
+      return (
+        this.$store.state.groups[
+          this.$store.state.groups.findIndex((v) => v.id == this.groupID)
+        ] || { training_direction: {} }
+      );
+    },
+  },
+  watch:{
+    $route(){
+      this.disciplines = this.name.disciplines || [];
     }
   },
-  methods:{
-    closeModal(){
-      var q = {...this.$route.query}
-      this.$delete(q,"id")
-      this.$delete(q,"modalType")
+  methods: {
+    closeModal() {
+      var q = { ...this.$route.query };
+      this.$delete(q, "id");
+      this.$delete(q, "modalType");
       this.$router.replace({
         name: this.$route.name,
-        query: q
-      })
+        query: q,
+      });
+    },
+    async saveGroup() {
+      if (this.name.id){
+        this.name.disciplines = this.disciplines;
+        this.name.flow_id = this.name.flow.id;
+        this.name.training_direction_id = this.name.training_direction.id;
+        await http.updateItem('Group', this.name.id, this.name, true);
+      }
+      this.closeModal();
     }
+  },
+  async beforeMount() {
+    if(this.$store.state.disciplines.length == 0){
+      await this.$store.dispatch("setDisciplines");
+    }
+    this.disciplines = this.name.disciplines || [];
   }
 };
 </script>
--- src/components/modals/ModalTeacher.vue
+++ src/components/modals/ModalTeacher.vue
@@ -1,5 +1,5 @@
 <template lang="">
-    <b-modal  id="modalTeacher" :title="name.last_name+' '+name.first_name+' '+name.middle_name" @hidden="closeModal()">
+    <b-modal  id="modalTeacher" @ok="saveTeacher" :title="name.last_name+' '+name.first_name+' '+name.middle_name" @hidden="closeModal()">
         <div class="d-block">
           <b-input-group size="md" :prepend="'Фамилия'">
             <b-form-input type="text" :value="name.last_name"></b-form-input>
@@ -20,18 +20,43 @@
             <b-form-input type="number" :value="name.total_hours"></b-form-input>
           </b-input-group>
         </div>
+        <div class="d-block">
+        <label>Дисциплины</label>
+        <v-select
+          multiple
+          v-model="disciplines"
+          id="disciplines"
+          :options="$store.state.disciplines"
+          placeholder="Выберите дисциплину"
+          label='title'
+          value='id'
+        ></v-select>
+      </div>
     </b-modal>
 </template>
 <script>
+import "vue-select/dist/vue-select.css";
+import vSelect from "vue-select";
+import http from '../../http';
+
 export default {
   props: ["teacherID"],
+  components: {
+    vSelect,
+  },
   data(){
     return{
+      disciplines: [],
     }
   },
   computed:{
     name(){
       return this.$store.state.teachers[this.$store.state.teachers.findIndex(v => v.id == this.teacherID)] || {}
+    }
+  },
+  watch:{
+    $route(){
+      this.disciplines = this.name.disciplines || [];
     }
   },
   methods:{
@@ -43,7 +68,20 @@ export default {
         name: this.$route.name,
         query: q
       })
+    },
+    async saveTeacher() {
+      if (this.name.id){
+        this.name.disciplines = this.disciplines;
+        await http.updateItem('Teacher', this.name.id, this.name, true);
+      }
+      this.closeModal();
     }
+  },
+  async beforeMount() {
+    if(this.$store.state.disciplines.length == 0){
+      await this.$store.dispatch("setDisciplines");
+    }
+    this.disciplines = this.name.disciplines || [];
   }
 };
 </script>
