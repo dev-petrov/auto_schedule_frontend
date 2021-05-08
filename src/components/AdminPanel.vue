a8d59a9b8e71fdd8cf6a8c1657e650b59b97462b
kirilko <chipzz87@gmail.com>
2021-05-08 16:57:26 +0300

    administration entities

---
+++ src/components/AdminPanel.vue
@@ -0,0 +1,51 @@
+<template lang="">
+    <div class='container'>
+        <b-tabs content-class="mt-3" fill>
+            <b-tab title="Образовательный план" @click="
+                $router.replace({
+                  name: $route.name,
+                  query: {
+                    ...$route.query,
+                    type: 'EducationPlan',
+                  },
+                })
+              "><EducationPlan/></b-tab>
+            <b-tab title="Преподаватели" @click="
+                $router.replace({
+                  name: $route.name,
+                  query: {
+                    ...$route.query,
+                    type: 'teachers',
+                  },
+                })
+              "><Teachers/></b-tab>
+            <b-tab title="Группы" @click="
+                $router.replace({
+                  name: $route.name,
+                  query: {
+                    ...$route.query,
+                    type: 'groups',
+                  },
+                })
+              "><Groups/></b-tab>
+            <b-tab title="Disabled" disabled><p>I'm a disabled tab!</p></b-tab>
+        </b-tabs>
+    </div>
+</template>
+<script>
+import EducationPlan from "./EducationPlanTable.vue";
+import Teachers from "./TeachersTable.vue"
+import Groups from "./GroupsTable.vue"
+export default {
+  components: {
+    EducationPlan,
+    Teachers,
+    Groups
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="">
+</style>
---
+++ src/components/GroupsTable.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <v-select
+      multiple
+      v-model="groups"
+      id="disciplines"
+      :options="$store.state.groups"
+      placeholder="Выберите группы"
+      label="code"
+      :reduce="(v) => v.id"
+      value="id"
+    ></v-select>
+    <b-button
+      @click="$bvModal.show('modalGroup')"
+      block
+      variant="primary"
+      >Добавить</b-button
+    >
+    <b-table :items="group" :fields="fields">
+      <template #cell(edit)="row">
+        <b-button
+          class="btn btn-primary"
+          @click="
+            $router.replace({
+              name: $route.name,
+              query: {
+                ...$route.query,
+                id: row.item.id,
+                modalType: 'modalGroup',
+              },
+            })
+          "
+        >
+          Редактировать
+        </b-button>
+      </template>
+    </b-table>
+    <ModalGroup />
+  </div>
+</template>
+
+<script>
+import "vue-select/dist/vue-select.css";
+import vSelect from "vue-select";
+import ModalGroup from "./modals/ModalGroup.vue";
+export default {
+  components: { ModalGroup, vSelect },
+  data() {
+    return {
+      fields: [
+        {
+          key: "code",
+          label: "Группа",
+          formatter: (value) => {
+            console.log(this.group)
+            return value
+          },
+        },
+        {
+          key: "count_of_students",
+          label: "Размер",
+          formatter: (value) => {
+            return value
+          },
+        },
+        {
+          key: "flow",
+          label: "Направление",
+          formatter: (value) => {
+              return value.name
+          }
+        },
+        {
+          key: "disciplines",
+          label: "Дисциплины",
+          formatter: (value) => {
+              let arr = []
+              for (var i in value){
+                  arr.push(value[i].title)
+              }
+              return arr.toString().replace("[", "").replace("]", "")
+          }
+        },
+        {
+          key: "edit",
+          label: "",
+        },
+      ],
+      groups: [],
+    };
+  },
+  async beforeMount() {
+    if (this.$store.state.groups.length == 0) {
+      await this.$store.dispatch("setGroups");
+    }
+    if (this.$route.query.modalType === "modalGroup") {
+      this.$bvModal.show("modalGroup");
+    }
+  },
+  computed: {
+    group() {
+      return this.$store.state.groups.filter((v) =>
+        this.groups.length != 0 ? this.groups.includes(v.group.id) : true
+      );
+    },
+  },
+  watch: {
+    $route() {
+      if (this.$route.query.modalType === "modalGroup") {
+        this.$bvModal.show("modalGroup");
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
