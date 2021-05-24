e7abf618d7c5c23609e273a5f4aa5888712eceea
kirilko <chipzz87@gmail.com>
2021-05-24 19:27:03 +0300

    redact fix

--- src/components/AdminPanel.vue
+++ src/components/AdminPanel.vue
@@ -5,7 +5,7 @@
         <Groups v-if="this.$route.query.type=='groups'"/>
         <TrainingDirection v-if="this.$route.query.type=='training_direction'"/>
         <DisciplinesTable v-if="this.$route.query.type=='disciplines'"/>
-        <RoomTable v-if="this.$route.query.type=='lecture_hall'"/>
+        <LectureHall v-if="this.$route.query.type=='lecture_hall'"/>
     </div>
 </template>
 <script>
@@ -14,7 +14,7 @@ import Teachers from "./TeachersTable.vue"
 import Groups from "./GroupsTable.vue"
 import TrainingDirection from "./TrainingDirectionTable.vue"
 import DisciplinesTable from "./DisciplinesTable.vue"
-import RoomTable from "./LectureHallTable.vue"
+import LectureHall from "./LectureHallTable.vue"
 export default {
   components: {
     EducationPlan,
@@ -22,7 +22,7 @@ export default {
     Groups,
     TrainingDirection,
     DisciplinesTable,
-    RoomTable
+    LectureHall
   },
   data() {
     return {};
